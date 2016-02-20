0f54be720e843b684cca6f19c60cabf9e1670196
cym0n <cymon@tiscali.it>
2016-02-20 21:44:19 +0100

    Template refactoring and navigations

--- BopWeb/lib/BopWeb.pm
+++ BopWeb/lib/BopWeb.pm
@@ -12,36 +12,50 @@ my $root_path = abs_path($module_file_path);
 $root_path =~ s/lib\/BopWeb\.pm//;
 my $metadata_path = $root_path . "metadata";
 
-my @reports = ('situation', 'hotspots', 'alliances', 'influences', 'supports', 'rebel-supports', 'war-history' );
+my @reports = ('situation', 'hotspots', 'alliances', 'influences', 'supports', 'rebel-supports', 'war-history', 'events' );
 my %report_names = ('situation' => 'Situation',
                     'hotspots' => 'Hotspots',
                     'alliances' => 'Alliances',
                     'influences' => 'Influences',
                     'supports' => 'Military Supports',
                     'rebel-supports' => 'Rebel Supports',
-                    'war-history' => 'War History');
+                    'war-history' => 'War History',
+                    'events' => 'Events'
+                );
+my @nation_reports = ('actual', 'borders', 'near', 'diplomacy', 'events' );
+my %nation_report_names = ('actual' => 'Status',
+                           'borders' => 'Borders',
+                           'near' => 'Military Range',
+                           'diplomacy' => 'Diplomacy',
+                           'events' => 'Events' 
+                    );
+
 
 get '/' => sub {
     template 'home';
 };
 
-get '/:game' => sub {
-    my $meta = $metadata_path . '/' . params->{game} . '.meta';
-    debug "Game found";
-    if(-e $meta)
+get '/play/:game' => sub {
+    my $meta = get_meta(params->{game});
+    if($meta)
     {
-        open my $metafile, '<', $meta || die $!;
-        debug "Opening $meta";
-        my $data;
-        {
-            local $/;    # slurp mode
-            my $metafile_data = <$metafile>;
-            debug "In the file: $metafile_data";
-            my $VAR1;
-            eval $metafile_data;
-            debug Dumper($data);
-            my $redirection = "/" . params->{game} . "/" . $VAR1->{'current_year'} . "/situation";
-            redirect $redirection, 301;
+        my $redirection = "/play/" . params->{game} . "/" . $meta->{'current_year'} . "/r/situation";
+        redirect $redirection, 301;
+    }
+    else
+    {
+        pass;
+    }
+};
+get '/play/:game/n' => sub {
+    my $meta = get_meta(params->{game});
+    if($meta)
+    {
+        template 'nations', {
+            areas => ['North America', 'South America', 'Europe', 'Africa', 'Middle East', 'Far East', 'Pacific'],
+            nations => $meta->{nations},
+            game => params->{game},
+            year => $meta->{'current_year'}
         }
     }
     else
@@ -50,13 +64,13 @@ get '/:game' => sub {
     }
 };
 
-get '/:game/:year/:turn/:report' => sub {
+get '/play/:game/:year/:turn/r/:report' => sub {
     my $report_to_show = 'generated/' . 
                          params->{game} . '/' .
                          params->{year} . '/' .
                          params->{turn} . '/' .
                          params->{report} . '.tt'; 
-    template 'general', {
+    template 'report', {
        'report' => $report_to_show,
        'reports' => \@reports,
        'report_names' => \%report_names,
@@ -66,6 +80,52 @@ get '/:game/:year/:turn/:report' => sub {
        'turn' => params->{turn}
     }; 
 };
+get '/play/:game/:year/:turn/n/:nation' => sub {
+        my $redirection = "/play/" . params->{game} . "/" . params->{year} . "/" . params->{turn} . "/n/" . params->{nation} . "/actual";
+        redirect $redirection, 301;
+};
 
 
+get '/play/:game/:year/:turn/n/:nation/:report' => sub {
+    my $report_to_show = 'generated/' . 
+                         params->{game} . '/' .
+                         params->{year} . '/' .
+                         params->{turn} . '/' .
+                         params->{nation} . '/'.
+                         params->{report} . '.tt'; 
+    template 'nation_report', {
+       'report' => $report_to_show,
+       'reports' => \@nation_reports,
+       'report_names' => \%nation_report_names,
+       'active' => params->{report},
+       'game' => params->{game},
+       'year' => params->{year},
+       'nation' => params->{nation},
+       'turn' => params->{turn}
+    }; 
+
+};
+
+
+sub get_meta
+{
+    my $game = shift;
+    my $meta = $metadata_path . '/' . $game . '.meta';
+    if(-e $meta)
+    {
+        open my $metafile, '<', $meta || die $!;
+        my $data;
+        {
+            local $/;    # slurp mode
+            my $metafile_data = <$metafile>;
+            my $VAR1;
+            eval $metafile_data;
+            return $VAR1;
+        }
+    }
+    else
+    {
+        return undef;
+    }
+}
 true;
