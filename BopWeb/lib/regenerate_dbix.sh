400a3c0d110b8744c1e9839aa25eb512e7e6ae35
cymon <cymon@volkov>
2016-08-07 16:04:21 +0200

    DBI script

---
+++ BopWeb/lib/regenerate_dbix.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#dbicdump -o dump_directory=. -o components='["InflateColumn::DateTime"]' -o overwrite_modifications=true DjakaWeb::DjakartaDB dbi:SQLite:../DB/djaka.db  '{ quote_char => "\"" }'
+dbicdump -o dump_directory=. -o components='["InflateColumn::DateTime"]' BopWeb::BopWebDB dbi:SQLite:../test.sqlite  
+
