3348826839f89ff3573b23e11cf67352cbbbbdc5
cymon <cymon@volkov>
2016-08-30 01:24:19 +0200

    test mission in the database

---
+++ sql/TEST_MISSION.sql
@@ -0,0 +1,11 @@
+INSERT INTO BOP_MISSIONS VALUES
+(NULL,
+'parcel',
+NULL, 
+'1987/3',
+1,
+'{"assignment":"Italy","from":"Chile","to":"Indonesia"}',
+'{"money":100,"friendship":{"assignment":3,"from":-2,"to":1}}',
+"Italy",
+0);
+
